99bcf1748188bf6168ba10d4c93ce5d89d838934
Francisco Javier Ribó Labrador <elribonazo@gmail.com>
2024-05-19 00:16:17 +0200

    fix: releasing

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ is_mac() {
   [[ "$OSTYPE" == "darwin"* ]]
 }
 
-wasm-pack build --target=web --out-dir="pkg"
+wasm-pack build --target=web --out-dir=pkg
 
+cd pkg
 if is_mac; then
-  sed -i '' "/if (typeof input === 'undefined') {/,/}/d" "pkg/undebugger_wasm.js"
+  sed -i '' "/if (typeof input === 'undefined') {/,/}/d" undebugger_wasm.js
 else
-  sed -i "/if (typeof input === 'undefined') {/,/}/d" "pkg/undebugger_wasm.js"
+  sed -i "/if (typeof input === 'undefined') {/,/}/d" undebugger_wasm.js
 fi
