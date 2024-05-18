959de02efb1ccdbbce37722aa454000d7545c924
Francisco Javier Ribó Labrador <elribonazo@gmail.com>
2024-05-18 22:45:21 +0200

    fix: add to ignore Signed-off-by: Francisco Javier Ribó Labrador <elribonazo@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -14,44 +14,29 @@ pub fn main() -> Result<(), JsValue> {
         console.warn = function() {};
         console.error = function() {};
         console.info = function() {};
-        const detect = (function(){
-            const dummy = /./;
-            dummy.toString = () => {
-                kfunction();
-                return 'SOME_NAME';
-            };
-            return dummy;
-        }());
     }"#).expect("Failed to add function to window");
-
     js_sys::eval(r#"window.kfunction = function kfunction() {
         debugger;
     }"#).expect("Failed to add function to window");
-
     let l_func = js_sys::Reflect::get(&window().expect("no global `window` exists"), &JsValue::from_str("lfunction"))
         .expect("undebugger function should be defined")
         .dyn_into::<Function>()
         .expect("undebugger should be a function");
-
     let _l = l_func.call0(&JsValue::NULL);
-
     fn call_debugger_repeatedly() {
         let k_func = js_sys::Reflect::get(&window().expect("no global `window` exists"), &JsValue::from_str("kfunction"))
             .expect("undebugger function should be defined")
             .dyn_into::<Function>()
             .expect("undebugger should be a function");
         let _ = k_func.call0(&JsValue::NULL);
-
         let callback = Closure::wrap(Box::new(move || {
             let _ = k_func.call0(&JsValue::NULL);
         }) as Box<dyn FnMut()>);
-
         window().expect("no global `window` exists")
             .set_interval_with_callback_and_timeout_and_arguments_0(callback.as_ref().unchecked_ref(), 1)
             .expect("Failed to set interval");
         callback.forget();
     }
-
     call_debugger_repeatedly();
     Ok(())
 }
