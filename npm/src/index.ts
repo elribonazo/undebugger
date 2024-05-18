d7326ded78d316e363eeaa4d5e331860170af40a
Francisco Javier Ribó Labrador <elribonazo@gmail.com>
2024-05-19 00:10:28 +0200

    fix: build

--- npm/src/index.ts
+++ npm/src/index.ts
@@ -2,9 +2,9 @@
 async function Undebugger() {
     const Undebugger = await import("undebugger-wasm");
     const pkgWasm = await import("undebugger-wasm/undebugger_wasm_bg.wasm");
-    return Undebugger.default(await (pkgWasm as any).default())
+    return Undebugger.default(
+        await (pkgWasm as any).default()
+    )
 }
-
 Undebugger()
-
 export {}
